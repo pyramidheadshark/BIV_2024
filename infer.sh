9e2c8800dfc7d1fe6f4046418ebd05daf5cf81e3
Chaberis <chaberwork@mail.ru>
2024-11-16 23:43:47 +0300

    add Dockerfile

--- infer.sh
+++ infer.sh
@@ -1,7 +1,6 @@
 python3 src/models_dev/q_infer.py --file_path data/raw/payments_main.tsv \
-                 --model_path models/onnx_model/model_quantized.onnx \
-                 --tokenizer_path models/onnx_model \
-                 --output_file data/predictions.tsv \
-                 --batch_size 64 \
-                 --max_length 128
-
+                                    --model_path models/onnx_model/model_quantized.onnx \
+                                    --tokenizer_path models/onnx_model \
+                                    --output_file data/predictions.tsv \
+                                    --batch_size 64 \
+                                    --max_length 128
