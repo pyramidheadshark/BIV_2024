8470b0789cd39bae3917a9be9114cf2babad2a30
Chaberis <chaberwork@mail.ru>
2024-11-17 00:44:21 +0300

    reworked infer file_path

--- infer.bat
+++ infer.bat
@@ -1,5 +1,5 @@
 python src\models_dev\q_infer.py ^
-    --file_path payments_main.tsv ^
+    --file_path data\payments_main.tsv ^
     --model_path models\onnx_model\model_quantized.onnx ^
     --tokenizer_path models\onnx_model ^
     --output_file data\predictions.tsv ^
--- infer.sh
+++ infer.sh
@@ -1,4 +1,4 @@
-python3 src/models_dev/q_infer.py --file_path payments_main.tsv \
+python3 src/models_dev/q_infer.py --file_path data/payments_main.tsv \
                                     --model_path models/onnx_model/model_quantized.onnx \
                                     --tokenizer_path models/onnx_model \
                                     --output_file data/predictions.tsv \
