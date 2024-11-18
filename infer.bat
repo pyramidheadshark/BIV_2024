88994cc9b9983b2acabf525993ea3888b5e8c546
“pyramidheadshark” <notevengael@gmail.com>
2024-11-18 13:32:24 +0300

    Updated .gitignore

--- infer.bat
+++
@@ -1,7 +0,0 @@
-python src\models_dev\q_infer.py ^
-    --file_path data\payments_main.tsv ^
-    --model_path models\onnx_model\model_quantized.onnx ^
-    --tokenizer_path models\onnx_model ^
-    --output_file data\predictions.tsv ^
-    --batch_size 64 ^
-    --max_length 128
